127d007eff8b9d76970cbe479328a14c40f4c95a
mawia <92118320+39LPeter@users.noreply.github.com>
2024-05-28 18:28:28 +0300

    fix

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,5 +1,9 @@
-er class
+
+#!/usr/bin/python3
 """
+User class
+"""
+
 
 class User():
     """ Documentation """
@@ -20,8 +24,13 @@ class User():
             raise TypeError("email must be a string")
         self.__email = value
 
-  
-    
+    """@property
+    def email(self):
+        """ """Documentation""" """
+        return self.__email
+    """
+
+
 if __name__ == "__main__":
 
     u = User()
